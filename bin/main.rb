999fc742ef322a87c91008fe542b071be4d1a560
enkog <nkirukaogu@gmail.com>
2020-09-29 07:29:22 +0100

    create instance variables

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
-puts 'Hello world!'
+class UserInterface
+  puts "WELCOME TO TIC-TAC-TOE GAME"
+  puts
+  attr_accessor :player_one, :player_two, :board
+
+  def initialize(player_one, player_two)
+    @board = %w[# # # # # # # # #]
+    @player_one = player_one
+    @player_two = player_two
+    @token = ['X','O']
+  end
+end
