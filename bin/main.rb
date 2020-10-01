9ff675e622c19eb211858a56f833c6a9ec79e09e
enkog <nkirukaogu@gmail.com>
2020-10-01 14:55:20 +0000

    Effect corrections from TSEs

--- bin/main.rb
+++ bin/main.rb
@@ -20,9 +20,9 @@ puts 'Player 2, input name: '
 @player_two = gets.chomp
 
 # allow first player choose a game token X or O
-puts "#{player_one}, Choose a token: 'X' or 'O'"
-
-while selected_token == gets.chomp
+puts "#{@player_one}, Choose a token: 'X' or 'O'"
+selected_token = gets.chomp
+while selected_token
   selected_token = selected_token.upcase
 
   # validate the token chosen
@@ -37,35 +37,40 @@ while selected_token == gets.chomp
                                       @game_tokens[0]
                                     end
     puts 'Tokens assigned successfully'
-    puts "#{player_one}'s Token:  #{@assigned_tokens[:player_one]}"
-    puts "#{player_two}'s Token:  #{@assigned_tokens[:player_two]}"
+    puts "#{@player_one}'s Token:  #{@assigned_tokens[:player_one]}"
+    puts "#{@player_two}'s Token:  #{@assigned_tokens[:player_two]}"
     break
   end
 end
 
-# display gameboard with available moves
-# prompt current player to select a move which is an index
-puts 'Enter a position: '
-position = gets.chomp
-puts position
+game_on = true
+while game_on
+  # display gameboard with available moves
+  # prompt current player to select a move which is an index
+  puts 'Enter a position: '
+  position = gets.chomp
+  puts position
 
-# validate the position. If already taken or if out of available board index,
-# display message and prompt for the input again.
+  # validate the position. If already taken or if out of available board index,
+  # display message and prompt for the input again.
 
-# if position is valid, update board
-board[index] = currentPlayersToken
+  # if position is valid, update board
+  board[index] = currentPlayersToken
 
-# switch to next player's turn
-# method for player turn will be called here,
-# which will return the next player name whose turn it is to play.
+  # switch to next player's turn
+  # method for player turn will be called here,
+  # which will return the next player name whose turn it is to play.
 
-# if current player move is up to three turns
-# check for winning combination
-# if current move matches winning combination
-puts 'That is a winning combination'
-# display winning player's name, end game
+  # if current player move is up to three turns
+  # check for winning combination
+  # if current move matches winning combination
+  puts 'That is a winning combination'
+  # display winning player's name, end game
 
-# otherwise pass turn to next player if board is not full
-# if board is full check winning combination again to determine winner
-# if no winner can be determined, then declare a draw.
-puts 'Thanks for playing'
+  # otherwise pass turn to next player if board is not full
+  # if board is full check winning combination again to determine winner
+  # if no winner can be determined, then declare a draw.
+  puts 'Thanks for playing'
+
+  game_on = false if winner
+end
